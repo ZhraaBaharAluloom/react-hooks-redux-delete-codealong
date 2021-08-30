41d2f59d88a5149fca716919d0ea9af401bc75c3
Zahraa <alwarda@hotmail.co.uk>
2021-08-30 14:11:00 -0700

    create a delete button to remove an items from redux state

--- src/features/todos/Todo.js
+++ src/features/todos/Todo.js
@@ -1,7 +1,22 @@
 import React from "react";
 
-function Todo({ text }) {
-  return <li>{text}</li>;
+// Redux
+import { useDispatch } from "react-redux";
+import { todoRemoved } from "./todosSlice";
+
+function Todo({ todo }) {
+  const dispatch = useDispatch();
+
+  const handleDelete = () => {
+    console.log(todo.id);
+    dispatch(todoRemoved(todo.id));
+  };
+  return (
+    <li>
+      {todo.text}
+      <button onClick={handleDelete}>Remove</button>
+    </li>
+  );
 }
 
 export default Todo;
